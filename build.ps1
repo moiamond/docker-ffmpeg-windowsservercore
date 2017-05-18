2ef6cc9a20b2c11bbebece7009581c7ae04b70e3
moiamond <moiamond@gmail.com>
2017-05-18 12:25:45 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ $files = ""
 Write-Host Starting build
 
 Write-Host Updating base images
-docker moiamond/ffmpeg-base-windowsservercore
+docker pull moiamond/ffmpeg-base-windowsservercore
 
 docker build -t moiamond/ffmpeg-windowsservercore .
 
